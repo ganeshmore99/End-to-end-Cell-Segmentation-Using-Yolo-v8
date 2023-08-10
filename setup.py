b69cbfd3af2b056ce0f0d3a64c6104cb0670edbb
ganeshmore99 <ganeshmore5199@gmail.com>
2023-08-10 11:11:58 +0530

    Initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Ganesh More',
+    author_email= 'ganeshmore5199@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
